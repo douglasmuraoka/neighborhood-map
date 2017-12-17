ff2c1c40cdffc6ff1dcff8b2645de7ca058cedf2
Douglas Muraoka <douglas.muraoka@gmail.com>
2017-12-16 23:15:54 -0200

    refactor: Change binding context to root
    Changes the binding context to root, instead to map property
    of the MapViewModel. That makes more sense than binding
    to a property which in fact was just the parameters used
    to construct the Google Map.

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,9 @@
 var MapViewModel = function (lat, lng, zoom) {
 	var self = this;
 
-	this.map = ko.observable({
+	this.map = null; // GoogleMap
+
+	this.mapOptions = ko.observable({
 		'lat': ko.observable(lat),
 		'lng': ko.observable(lng),
 		'zoom': ko.observable(zoom)
@@ -12,13 +14,15 @@ var MapViewModel = function (lat, lng, zoom) {
 // Custom Binding for Google Map
 ko.bindingHandlers.googlemap = {
 	init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-		// Recovers model binded
-		var model = valueAccessor()();
+		// Recovers ViewModel binded
+		var viewModel = ko.unwrap(valueAccessor());
 
 		// Initialization of our map with data from MapViewModel
-		var googleMap = new google.maps.Map(element, {
-			'center': new google.maps.LatLng(model.lat(), model.lng()),
-			'zoom': model.zoom()
+		var mapOptions = viewModel.mapOptions();
+		var location = new google.maps.LatLng(mapOptions.lat(), mapOptions.lng());
+		viewModel.map = new google.maps.Map(element, {
+			'center': location,
+			'zoom': mapOptions.zoom()
 		});
 	}
 };
